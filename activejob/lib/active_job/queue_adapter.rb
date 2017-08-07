a1ce5176c65b1a9736eee260c26553dbb7f47a60
David Reese <david@whatcould.com>
2017-08-06 20:52:27 -0400

    Allow setting queue_adapter on per-job basis

--- activejob/lib/active_job/queue_adapter.rb
+++ activejob/lib/active_job/queue_adapter.rb
@@ -2,27 +2,32 @@ require 'active_job/queue_adapters/inline_adapter'
 require 'active_support/core_ext/string/inflections'
 
 module ActiveJob
-  # The <tt>ActionJob::QueueAdapter</tt> module is used to load the
-  # correct adapter. The default queue adapter is the :inline queue.
   module QueueAdapter #:nodoc:
     extend ActiveSupport::Concern
 
-    # Includes the setter method for changing the active queue adapter.
-    module ClassMethods
-      mattr_reader(:queue_adapter) { ActiveJob::QueueAdapters::InlineAdapter }
+    included do
+      class_attribute :queue_adapter, instance_accessor: false
 
+      class << self
+        alias_method_chain :queue_adapter=, :interpretation
+      end
+
+      self.queue_adapter = :inline #set default queue_adapter to be :inline
+    end
+
+    module ClassMethods
       # Specify the backend queue provider. The default queue adapter
       # is the :inline queue. See QueueAdapters for more
       # information.
-      def queue_adapter=(name_or_adapter)
-        @@queue_adapter = \
+      def queue_adapter_with_interpretation=(name_or_adapter)
+        self.queue_adapter_without_interpretation= \
           case name_or_adapter
           when :test
             ActiveJob::QueueAdapters::TestAdapter.new
           when Symbol, String
             load_adapter(name_or_adapter)
-          else
-            name_or_adapter if name_or_adapter.respond_to?(:enqueue)
+          when Class
+            name_or_adapter
           end
       end
 
